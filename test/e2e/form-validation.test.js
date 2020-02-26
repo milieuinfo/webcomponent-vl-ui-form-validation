de0b12a5afa77164265edf086c841b8269314c18
coemans <tom.coemans@gmail.com>
2020-02-26 13:52:03 +0100

    vl-ui-form-grid and vl-ui-input-field update (#16)

--- test/e2e/form-validation.test.js
+++ test/e2e/form-validation.test.js
@@ -2,15 +2,13 @@ const { assert, driver } = require('vl-ui-core').Test.Setup;
 const VlFormValidationPage = require('./pages/vl-form-validation.page');
 
 describe('vl-form-validation', async () => {
-//    const vlFormValidationPage = new VlFormValidationPage(driver);
-//
-//    before((done) => {
-//        vlFormValidationPage.load().then(() => {
-//            done();
-//        });
-//    });
+   const vlFormValidationPage = new VlFormValidationPage(driver);
 
-    // it("Dummy test om de browsers te sluiten", () => {
-    // 	assert.isTrue(true);
-    // });
+   before((done) => {
+       return vlFormValidationPage.load();
+   });
+
+    it("Dummy test om de browsers te sluiten", () => {
+    	assert.isTrue(true);
+    });
 });
