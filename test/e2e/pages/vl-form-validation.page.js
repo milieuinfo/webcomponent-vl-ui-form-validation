d5a0f8a96e70247f3e8dfee0c9bdee180319e75f
wautergu <guy.wauters@vlaanderen.be>
2020-03-09 14:08:33 +0100

    dummy compontent uit page object halen

--- test/e2e/pages/vl-form-validation.page.js
+++ test/e2e/pages/vl-form-validation.page.js
@@ -1,8 +1,5 @@
-const VlFormValidation = require('../components/vl-form-validation');
-
-const { Page, Config, VlElement } = require('vl-ui-core').Test;
-const { By } = require('vl-ui-core').Test.Setup;
-const { Key } = require('selenium-webdriver');
+const { Page, Config } = require('vl-ui-core').Test;
+const  DummyForm = require('../components/dummy-components');
 
 
 class VlFormValidationPage extends Page {
@@ -48,60 +45,4 @@ class VlFormValidationPage extends Page {
     }
 }
 
-class DummyForm extends VlElement {
-
-    async getInputField() {
-        const inputField =  await this.findElement(By.tagName('input'));
-        return new DummyInputValidationField(this.driver, inputField);
-    }
-
-    async getErrorMessage() {
-        const errorMessage = await this.findElement(By.css('.error-message'));
-        return new DummyFormValidationMessage(this.driver, errorMessage);
-    }
-
-    async getSelectField() {
-        const selectField = await this.findElement(By.tagName('select'));
-        return new DummySelectValidationField(this.driver, selectField);
-    }
-}
-
-class DummyInputValidationField extends VlFormValidation { 
-    
-    async setInputValue(content) {
-        await this.clear();
-        return this.sendKeys(content);
-    }
-
-    async getInputValue() {
-        return this.getAttribute('value');
-    }
-
-    async blur() {
-        return this.sendKeys(Key.TAB);
-    }
-}
-
-class DummySelectValidationField extends VlFormValidation {
-
-    async selectByIndex(index) {
-        const options = await this.findElements(By.tagName('option'));
-        await options[index].click();
-        return this.blur();
-    }
-
-    async blur() {
-        return this.sendKeys(Key.TAB);
-    }
-}
-
-class DummyFormValidationMessage extends VlElement {
-
-    async getErrorId() {
-        return this.getAttribute('data-vl-error-id');
-    }
-   
-}
-
-module.exports = DummyInputValidationField;
 module.exports = VlFormValidationPage;
