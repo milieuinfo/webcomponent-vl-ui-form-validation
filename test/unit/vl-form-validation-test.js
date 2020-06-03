2dc055e0101f98f1ecc40cba813107197e1851a2
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 18:15:55 +0200

    fixed test

--- test/unit/vl-form-validation-test.js
+++ test/unit/vl-form-validation-test.js
@@ -6,16 +6,16 @@ import '/lib/form-validation.js';
 class VlFormValidationTest extends vlElement(HTMLElement) {
   constructor() {
     super(`
-            <form data-vl-validate-form>
-                <label class="vl-form__label" for="validation-naam">Naam</label>
-                <div class="vl-input-group">
-                    <input name="validation-naam" type="text" data-required="true" data-vl-success-class="success-style"
-                        data-vl-error-class="error-style" data-vl-error-message="Veld &quot;Naam&quot; is verplicht"
-                        data-vl-error-placeholder="validation-naam-error" aria-required="true" aria-invalid="true" />
-                </div>
-                <div class="error-message" data-vl-error-id="validation-naam-error"></div>
-            </form>
-        `);
+      <form data-vl-validate-form>
+          <label class="vl-form__label" for="validation-naam">Naam</label>
+          <div class="vl-input-group">
+              <input name="validation-naam" type="text" data-required="true" data-vl-success-class="success-style"
+                  data-vl-error-class="error-style" data-vl-error-message="Veld &quot;Naam&quot; is verplicht"
+                  data-vl-error-placeholder="validation-naam-error" aria-required="true" aria-invalid="true" />
+          </div>
+          <div class="error-message" data-vl-error-id="validation-naam-error"></div>
+      </form>
+    `);
   }
 
   get formElement() {
