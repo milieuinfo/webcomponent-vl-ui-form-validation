3c0090f1b0394053635d1bc0a67bc07d4256c626
Tom Coemans <tom.coemans@gmail.com>
2020-08-07 13:04:53 +0200

    3.11.5 upgrade

--- dist/vl-form-validation.src.js
+++ dist/vl-form-validation.src.js
@@ -1,7 +1,7 @@
 import {awaitUntil} from 'vl-ui-core';
 import '@govflanders/vl-ui-util/dist/js/util.js';
 import '@govflanders/vl-ui-core/dist/js/core.js';
-import 'vl-ui-form-validation/lib/form-validation.js';
+import '@govflanders/vl-ui-form-validation/dist/js/form-validation.js';
 
 /**
  * De formulier validatie mixin in combinatie met een input field verzekert dat bij het invullen van het formulier de input van de gebruiker geldig is.
