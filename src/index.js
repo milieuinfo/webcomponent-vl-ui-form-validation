2eb454c456f03ce2cff7895a26b481239564957d
coemans <tom.coemans@gmail.com>
2020-09-21 10:07:26 +0200

    added lowercase export (#59)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = {
+  vlFormValidation: require('../dist/vl-form-validation.src.js').vlFormValidation,
   VlFormValidation: require('../dist/vl-form-validation.src.js').VlFormValidation,
 };
