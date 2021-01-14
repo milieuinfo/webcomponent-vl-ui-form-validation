b5930a520d0da6f55534ca4a4b657967dc7816b6
Tom Coemans <tom.coemans@gmail.com>
2021-01-14 10:34:04 +0100

    update dev dependencies

--- demo/vl-select-demo.js
+++ demo/vl-select-demo.js
@@ -1,20 +1,7 @@
 import {define} from '/node_modules/vl-ui-core/dist/vl-core.js';
 import {VlSelect} from '/node_modules/vl-ui-select/dist/vl-select.js';
-import {vlFormValidation, vlFormValidationElement} from '/src/vl-form-validation-all.js';
+import {vlFormValidation} from '/src/vl-form-validation-all.js';
 
-Promise.all([
-  vlFormValidation.ready(),
-]).then(() => {
-  define('vl-select-demo', VlSelectDemo, {extends: 'select'});
-});
+Promise.all([vlFormValidation.ready()]).then(() => define('vl-select-demo', VlSelectDemo, {extends: 'select'}));
 
-export class VlSelectDemo extends vlFormValidationElement(VlSelect) {
-  static get _observedAttributes() {
-    return VlSelect._observedAttributes.concat(vlFormValidation._observedAttributes());
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this._dressFormValidation();
-  }
-}
+export class VlSelectDemo extends VlSelect {}
