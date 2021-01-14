57d085c65b13cb2d62cce5a0edec86e0e9569e07
coemans <tom.coemans@vlaanderen.be>
2021-01-14 11:00:53 +0100

    Update dev dependencies (#76)

--- demo/vl-datepicker-demo.js
+++ demo/vl-datepicker-demo.js
@@ -1,20 +1,7 @@
 import {define} from '/node_modules/vl-ui-core/dist/vl-core.js';
 import {VlDatepicker} from '/node_modules/vl-ui-datepicker/dist/vl-datepicker.js';
-import {vlFormValidation, vlFormValidationElement} from '/src/vl-form-validation-all.js';
+import {vlFormValidation} from '/src/vl-form-validation-all.js';
 
-Promise.all([
-  vlFormValidation.ready(),
-]).then(() => {
-  define('vl-datepicker-demo', VlDatepickerDemo);
-});
+Promise.all([vlFormValidation.ready()]).then(() => define('vl-datepicker-demo', VlDatepickerDemo));
 
-export class VlDatepickerDemo extends vlFormValidationElement(VlDatepicker) {
-  static get _observedAttributes() {
-    return VlDatepicker._observedAttributes.concat(vlFormValidation._observedAttributes());
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this._dressFormValidation();
-  }
-}
+export class VlDatepickerDemo extends VlDatepicker {}
--- demo/vl-input-field-demo.js
+++ demo/vl-input-field-demo.js
@@ -1,20 +1,7 @@
 import {define} from '/node_modules/vl-ui-core/dist/vl-core.js';
 import {VlInputField} from '/node_modules/vl-ui-input-field/dist/vl-input-field.js';
-import {vlFormValidation, vlFormValidationElement} from '/src/vl-form-validation-all.js';
+import {vlFormValidation} from '/src/vl-form-validation-all.js';
 
-Promise.all([
-  vlFormValidation.ready(),
-]).then(() => {
-  define('vl-input-field-demo', VlInputFieldDemo, {extends: 'input'});
-});
+Promise.all([vlFormValidation.ready()]).then(() => define('vl-input-field-demo', VlInputFieldDemo, {extends: 'input'}));
 
-export class VlInputFieldDemo extends vlFormValidationElement(VlInputField) {
-  static get _observedAttributes() {
-    return VlInputField._observedAttributes.concat(vlFormValidation._observedAttributes());
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this._dressFormValidation();
-  }
-}
+export class VlInputFieldDemo extends VlInputField {}
