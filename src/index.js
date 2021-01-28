5979739dbcbc6629feab40c3f1bbb33825a28934
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 18:29:57 +0100

    ES export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-export * from '../dist/vl-form-validation-all.src.js';
+import {vlFormValidation, VlFormValidation} from '../dist/vl-form-validation.src.js';
+import {vlFormValidationElement} from '../dist/vl-form-validation-element.src.js';
+
+export {
+  vlFormValidation,
+  VlFormValidation,
+  vlFormValidationElement,
+};
