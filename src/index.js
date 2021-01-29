2ee9f731204814fe5647da5b5e5ce8c6f49d2603
coemans <tom.coemans@vlaanderen.be>
2021-01-29 12:40:35 +0100

    CommonJS imports vervangen door ES varianten (#79)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-module.exports = {
-  vlFormValidation: require('../dist/vl-form-validation.src.js').vlFormValidation,
-  VlFormValidation: require('../dist/vl-form-validation.src.js').VlFormValidation,
-  vlFormValidationElement: require('../dist/vl-form-validation-element.src.js').vlFormValidationElement,
-};
+export * from '../dist/vl-form-validation-all.src.js';
